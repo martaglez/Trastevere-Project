6dbe9c17c5d758f2df2616853afdbd1b3e2131ee
chelesmjd <chelsmjdalac@gmail.com>
2026-05-05 20:14:51 +0200

    fix about

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hugod\Desktop\SOFTWARE\Trastevere-Project\venv
+set VIRTUAL_ENV=C:\Users\chels\GitHub\Trastevere-Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
